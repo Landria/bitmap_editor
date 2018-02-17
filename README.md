7733a47ad72e7670e29fe49e0fc6d558813815ff
Natalia Sergeeva <natalia.m.sergeeva@gmail.com>
2018-02-17 03:23:57 +0300

    Odd input specs

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 
 Module **BitmapEditor** is for image bitmap processing. It converts given instructions into an image.
 
-### Ruby v2.3.4
+Ruby v2.3.4
+
+
+# Commands
+
+There are 6 supported commands:
+
+1. **I N M** - Create a new M x N image with all pixels coloured white (O).
+2. **C** - Clears the table, setting all pixels to white (O).
+3. **L X Y C** - Colours the pixel (X,Y) with colour C.
+4. **V X Y1 Y2 C** - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
+5. **H X1 X2 Y C** - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+6. **S** - Show the contents of the current image
 
 # Installation
 
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -15,4 +15,7 @@ RSpec.describe BitmapEditor do
   it { expect(BitmapEditor::Processor.new.calculate('examples/show.txt').bitmap).to eq expected_output }
   it { expect { BitmapEditor::Processor.new.calculate('examples/unknownshow.txt') }.to raise_error(ArgumentError) }
   it { expect(BitmapEditor::Processor.new.calculate('examples/clear_bitmap.txt').bitmap.flatten.uniq).to eq ['O'] }
+  it { expect { BitmapEditor::Processor.new.calculate('examples/unknow_command.txt') }.to raise_error(ArgumentError) }
+  it { expect { BitmapEditor::Processor.new.calculate('examples/wrong_command_args.txt') }.to raise_error(ArgumentError) }
+  it { expect { BitmapEditor::Processor.new.calculate('examples/wrong_commands_args_type.txt') }.to_not raise_error }
 end
