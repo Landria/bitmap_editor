a05d35fd219be65133dbe1832d49d1055440e61d
Natalia Sergeeva <natalia.m.sergeeva@gmail.com>
2018-02-17 03:31:19 +0300

    Better errors

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -4,21 +4,21 @@ require_relative 'bitmap_editor/commands'
 
 module BitmapEditor
   class Processor
-    
+
     attr_accessor :bitmap
 
     include BitmapEditor::Commands
 
     def run(file)
       calculate(file)
-    rescue ArgumentError
-      puts "Please provide correct file"
+    rescue ArgumentError => e
+      puts "ArgumentError: #{e.message}"
     rescue Exception => e
       puts "Unexpected exception: #{e.message}"
     end
 
     def calculate(file)
-      raise ArgumentError if file.nil? || !File.exists?(file)
+      raise(ArgumentError, 'Please provide correct file') if file.nil? || !File.exists?(file)
   
       File.open(file).each do |line|
         perform(line.chomp)
@@ -32,7 +32,7 @@ module BitmapEditor
     def perform(line)
       args = line.split(/\s/)
       command = COMMANDS[args.shift]
-      raise ArgumentError unless command_valid?(command, args)
+      raise(ArgumentError, 'Check file instructions') unless command_valid?(command, args)
       args.empty? ? send(command) : send(command, args)
     end
   end
